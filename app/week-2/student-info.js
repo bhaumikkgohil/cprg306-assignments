7273047c3219f259a8f22d2383dd830edffee6ed
bhaumikkgohil <bhaumikkgohil@gmail.com>
2024-09-23 20:59:09 -0600

    Done with Week-2 Assignments
    Done with Week-2 Assignments

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Bhaumik Kalpesh Gohil</h1>
+      <p>
+        <Link href="https://github.com/bhaumikkgohil">
+          https://github.com/bhaumikkgohil
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
