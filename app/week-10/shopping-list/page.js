9c8a94f239ab8e9de756497484cec1fafca55570
bhaumikkgohil <bhaumikkgohil@gmail.com>
2024-11-17 21:47:39 -0700

    Adjusted Page UI

--- app/week-10/shopping-list/page.js
+++ app/week-10/shopping-list/page.js
@@ -71,7 +71,7 @@ const Page = () => {
   };
 
   return (
-    <main className="p-4 bg-gray-900 ">
+    <main className="p-4 bg-gray-900 h-screen ">
       <h1 className="text-3xl font-bold text-white mb-4">Shopping List</h1>
       <div className="flex">
         <div>
